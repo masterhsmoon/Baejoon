4f6fed43946509b7028ef22bbe4ccd7813c40812
masterhsmoon <111688835+masterhsmoon@users.noreply.github.com>
2022-09-12 10:18:32 +0900

    [Bronze IV] Title: 주사위 세개, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,14 @@
+a, b, c = map(int, input().split())
+if a==b==c:
+    print(10000+a*1000)
+elif a==b or b==c: 
+    print(1000+b*100)
+elif a==c:
+    print(1000+a*100)
+elif a!=b and a!=c and b!=c:
+    if a>b and a>c:
+        print(a*100)
+    elif b>a and b>c:
+        print(b*100)
+    elif c>a and c>b:
+        print(c*100)
